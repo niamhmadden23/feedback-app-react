6e66e4012b0370bef4565d89da7cac45cccc46a6
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-07 16:22:54 +0100

    Added delete functionality to cards

--- src/components /FeedbackItem.jsx
+++ src/components /FeedbackItem.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useState } from "react";
+import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
 
-export default function FeedbackItem({ item }) {
+export default function FeedbackItem({ item, handleDelete }) {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example feedback item");
 
   return (
-    <div className="card">
+    <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
