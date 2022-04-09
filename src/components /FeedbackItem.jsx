cc83c8c717c88e3da8d931ccfcaec1a4281d2ce0
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-09 15:23:15 +0100

    moved functions to context and edited feedback event

--- src/components /FeedbackItem.jsx
+++ src/components /FeedbackItem.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useState } from "react";
 import Card from "./shared/Card";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-export default function FeedbackItem({ item, handleDelete }) {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState("This is an example feedback item");
-
+export default function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   return (
     <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
+      <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="white" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/components /FeedbackList.jsx
+++ src/components /FeedbackList.jsx
@@ -3,7 +3,7 @@ import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ handleDelete }) {
+function FeedbackList() {
   const { feedback } = useContext(FeedbackContext);
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet...</p>;
@@ -12,7 +12,7 @@ function FeedbackList({ handleDelete }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
   );
